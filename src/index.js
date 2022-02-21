43ed080ff3a35af64f3f2531f62a945fb85c6d78
German Renato Cobian <germancobian@hotmail.com>
2022-02-21 11:47:13 -0800

    Resolve linter issues

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import './index.css';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 
